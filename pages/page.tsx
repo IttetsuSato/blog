55fea4709338584cec6f2a1180bab45bcfe87aa7
Ittetsu Sato <ittetsusato@gmail.com>
2023-02-26 11:35:37 +0900

    feat: supabaseと連携し、データを表示する

---
+++ pages/page.tsx
@@ -0,0 +1,33 @@
+// import { supabase } from 'src/lib/supabaseClient';
+
+import { supabase } from "@/lib/supabase";
+
+type Country = {
+  id: number;
+  name: string;
+};
+
+type Props = {
+  countries: Country[];
+};
+
+const Page = ({ countries }: Props) => {
+  return (
+    <ul>
+      {countries.map((country) => (
+        <li key={country.id}>{country.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export const getServerSideProps = async () => {
+  let { data } = await supabase.from('countries').select();
+  return {
+    props: {
+      countries: data,
+    },
+  };
+};
+
+export default Page;

709148042f6046a463cd6f8f35872e9fc041fa14
Ittetsu Sato <ittetsusato@gmail.com>
2023-03-21 18:47:42 +0900

    feat: create Drawer

--- components/Header/NormalHeader.tsx
+++ components/Header/NormalHeader.tsx
@@ -1,12 +1,14 @@
 import { HeaderBox } from "../Box/HeaderBox";
-import { Icon, IconButton, Spacer } from "@chakra-ui/react";
+import { IconButton, Spacer, useDisclosure } from "@chakra-ui/react";
 import { BsGearWideConnected } from "react-icons/bs";
+import { AdminDrawer } from "../Drawer/AdminDrawer";
 
 export type NormalHeaderProps = {
   //
 };
 
 export const NormalHeader: React.FC<NormalHeaderProps> = ({}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <HeaderBox>
       <Spacer />
@@ -15,7 +17,9 @@ export const NormalHeader: React.FC<NormalHeaderProps> = ({}) => {
         as={BsGearWideConnected}
         isRound
         size="sm"
+        onClick={onOpen}
       />
+      <AdminDrawer isOpen={isOpen} onClose={onClose} />
     </HeaderBox>
   );
 };
